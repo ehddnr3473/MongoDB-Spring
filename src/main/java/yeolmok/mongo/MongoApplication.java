31c22a9f8bf4e06d19d4a034ba356ff1f8961741
김동욱 <ehddnr73@naver.com>
2026-05-22 09:23:35 +0900

    기본 동작

--- src/main/java/yeolmok/mongo/MongoApplication.java
+++ src/main/java/yeolmok/mongo/MongoApplication.java
@@ -1,13 +1,22 @@
 package yeolmok.mongo;
 
-import org.springframework.boot.SpringApplication;
+import com.mongodb.client.MongoClients;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @SpringBootApplication
 public class MongoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MongoApplication.class, args);
+		MongoOperations mongoOperations = new MongoTemplate(MongoClients.create(), "database");
+		mongoOperations.insert(new Person("Yeolmok", 31));
+
+		System.out.println(mongoOperations.query(Person.class).matching(where("name").is("Yeolmok")).firstValue());
+
+		mongoOperations.dropCollection("person");
 	}
 
 }
